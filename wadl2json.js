eb28e973ffc1b41fc6b38daec9de711eac3705a0
rbelouin <rodolphe.belouin@gmail.com>
2014-04-08 16:57:00 +0200

    Convert from URL.

--- wadl2json.js
+++ wadl2json.js
@@ -1,5 +1,7 @@
 var parser = require("xml2json");
 var fs = require("fs");
+var http = require("http");
+var https = require("https");
 var format = require("./format.js");
 
 exports.string2json = function(wadl, options) {
@@ -15,4 +17,20 @@ exports.string2json = function(wadl, options) {
 exports.file2json = function(filename, options) {
   var content = fs.readFileSync(filename).toString();
   return exports.string2json(content, options);
+};
+
+exports.url2json = function(url, callback, options) {
+  var protocol = url.indexOf("https://") == 0 ? https : http;
+
+  protocol.get(url, function(res) {
+    var content = "";
+    res.on("data", function(data) {
+      content += data;
+    });
+    res.on("end", function() {
+      callback(null, exports.string2json(content, options));
+    });
+  }).on("error", function(error) {
+    callback(error);
+  });
 };
