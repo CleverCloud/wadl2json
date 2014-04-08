85ab44851b345f0837c521a5ea218da60437593a
rbelouin <rodolphe.belouin@gmail.com>
2014-04-08 23:11:15 +0200

    Add grunt-jsdoc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jasmine_node: {
       all: ["spec/"]
+    },
+    jsdoc: {
+      dist: {
+        src: ["wadl2json.js"]
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-jasmine-node");
+  grunt.loadNpmTasks("grunt-jsdoc");
 
   grunt.registerTask("test", ["jasmine_node"]);
 
--- wadl2json.js
+++ wadl2json.js
@@ -1,6 +1,8 @@
-var wadl2json = module.exports = (function() {
-  var wadl2json = {};
-
+/**
+ * A module generating a JSON representation of a WADL content
+ * @module wadl2json
+ */
+(function() {
   var fs = require("fs");
   var http = require("http");
   var https = require("https");
@@ -10,6 +12,7 @@ var wadl2json = module.exports = (function() {
   var parser = require("xml2json");
   var beautify = require("js-beautify").js_beautify;
 
+  var wadl2json = exports;
   wadl2json._fetchers = {};
   wadl2json._fetchers["http:"] = http;
   wadl2json._fetchers["https:"] = https;
@@ -20,7 +23,14 @@ var wadl2json = module.exports = (function() {
     stringify:  false
   };
 
-  wadl2json._methodsFromWADLResource = function(basePath, resource) {
+  /**
+   * Extract all methods from a parsed WADL content
+   * @private
+   * @param {string} basePath - path which will be prepended to resource path
+   * @param {object} resource - WADL <resource> tag
+   * @returns {array} all methods contained in resource
+   */
+  exports._methodsFromWADLResource = function(basePath, resource) {
     var path = basePath + (_.last(basePath) == "/" ? "":"/") + resource.path;
 
     var methods = _.map(resource.method || [], function(method) {
@@ -42,7 +52,13 @@ var wadl2json = module.exports = (function() {
     return methods.concat(_.map(resource.resource || [], _.partial(wadl2json._methodsFromWADLResource, path)));
   };
 
-  wadl2json._groupMethodsByPath = function(methods) {
+  /**
+   * Group methods by path
+   * @private
+   * @param {array} methods - methods returned by _methodsFromWADLResource
+   * @returns {object} methods grouped by path
+   */
+  exports._groupMethodsByPath = function(methods) {
     return _.chain(methods)
       .groupBy("path")
       .mapValues(function(methods) {
@@ -53,7 +69,13 @@ var wadl2json = module.exports = (function() {
       .value();
   };
 
-  wadl2json._sortMethodsByPathAndVerb = function(methodsByPath) {
+  /**
+   * Sort methods by path, and then by verb
+   * @private
+   * @param {object} methodsByPath - methods returned by _groupMethodsByPath
+   * @returns {object} same object, with keys sorted by name
+   */
+  exports._sortMethodsByPathAndVerb = function(methodsByPath) {
     var paths = _.chain(methodsByPath).keys().sortBy().value();
 
     return _.foldl(paths, function(sortedMethods, path) {
@@ -62,7 +84,13 @@ var wadl2json = module.exports = (function() {
     }, {});
   };
 
-  wadl2json.fromJSON = function(wadlJson, options) {
+  /**
+   * Generate a JSON representation from parsed WADL content
+   * @param {object} wadlJson - object representing wadl content
+   * @param {object} [options] - options
+   * @returns {object|string} JSON representation of given WADL content
+   */
+  exports.fromJSON = function(wadlJson, options) {
     var options = _.extend({}, wadl2json._defaultOptions, options);
 
     var app = wadlJson && wadlJson.application && wadlJson.application[0];
@@ -79,7 +107,13 @@ var wadl2json = module.exports = (function() {
     return methodsByPath;
   };
 
-  wadl2json.fromString = function(wadlString, options) {
+  /**
+   * Generate a JSON representation from raw WADL content
+   * @param {string} wadlString - raw WADL content
+   * @param {object} [options] - options
+   * @returns {object|string} JSON representation of given WADL content
+   */
+  exports.fromString = function(wadlString, options) {
     /* Remove XML header as xml2json is not able to parse it */
     var wadlString = wadlString.replace(/<\?[^<]*\?>/g, "");
     var wadlJson = parser.toJson(wadlString, {
@@ -90,13 +124,31 @@ var wadl2json = module.exports = (function() {
     return wadl2json.fromJSON(wadlJson, options);
   };
 
-  wadl2json.fromFile = function(filename, options) {
+  /**
+   * Generate a JSON representation from a WADL file
+   * @param {string} filename - name of a file containing WADL content
+   * @param {object} [options] - options
+   * @returns {object|string} JSON representation of given WADL content
+   */
+  exports.fromFile = function(filename, options) {
     var wadlString = fs.readFileSync(filename).toString();
 
     return wadl2json.fromString(wadlString, options);
   };
 
-  wadl2json.fromURL = function(wadlURL, callback, options) {
+  /**
+   * @callback requestCallback
+   * @param {object} error - Forwarded error if unreachable content or generation fail. May be null.
+   * @param {object|string} JSON representation of given WADL content
+   */
+
+  /**
+   * Generate a JSON representation from a remote WADL file
+   * @param {string} wadlURL - url of remote WADL content
+   * @param {requestCallback} callback - function called on process end
+   * @param {object} [options] - options
+   */
+  exports.fromURL = function(wadlURL, callback, options) {
     var protocol = url.parse(wadlURL).protocol;
     var fetcher = wadl2json._fetchers[protocol];
 
@@ -117,6 +169,4 @@ var wadl2json = module.exports = (function() {
       });
     }
   };
-
-  return wadl2json;
 })();
